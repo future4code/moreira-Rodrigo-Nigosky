fbbe5bf09fd517a0cfed2d0d52e96045c3396932
Rodrigo Nigosky <nigosky@yahoo.com.br>
2021-11-29 15:16:14 -0300

    update-variaveis

---
+++ modulo1/variaveis/template_javascript/index.js
@@ -0,0 +1,64 @@
+console.log ("hello world")
+
+//****exercicios escritos*****
+
+//***resposta exercicio 1*** - será impresso o string "105"
+
+//***resposta exercicio 2*** - será impresso o number 10 10 10
+
+/****resposta exercicio 3*****
+let horasDiarias = prompt("Quantas horas você trabalha por dia?")
+let ganhoDia = prompt("Quanto você recebe por dia?")
+alert(`Voce recebe ${ganhoDia/horasDiarias} por hora`)*/
+
+
+
+
+//****exercicio 1 codigo****
+
+/*const nome = null
+const idade = null
+console.log(typeof nome, typeof idade)*/
+//foi impresso object, pois a variavel nao tem tipo/valor conhecido
+
+/*const nome = prompt("Qual seu nome?")
+const idade = prompt("Qual sua idade?")
+console.log(typeof nome, typeof idade)*/
+//as variaveis receberam a denomicação string, pois receberam valores
+
+/*const nome = prompt("Qual seu nome?")
+const idade = prompt("Qual sua idade?")
+console.log ("Olá", nome, "você tem", idade, "anos")*/
+
+
+//****exercicio 2 codigo
+
+/*const roupaAzul = "sim"
+const carroVerde = "nao"
+const casaBranca = "sim"
+console.log ("Você está usando uma roupa azul hoje?", roupaAzul)
+console.log ("Você tem um carro verde?", carroVerde)
+console.log ("Você tem uma casa branca?", casaBranca)*/
+
+
+//****exercicio 3 codigo
+
+
+/*let a = 10
+let b = 25
+let c = null
+c = a
+a = b
+b = c
+console.log("O novo valor de a é", a) // O novo valor de a é 25
+console.log("O novo valor de b é", b) // O novo valor de b é 10*/
+
+
+//****desafio
+
+/*const numero1 = prompt ("coloque um numero X")
+const numero2 = prompt ("coloque um numero Y")
+const soma = numero1 + numero2
+console.log (soma)
+console.log(numero1 * numero2)*/
+
