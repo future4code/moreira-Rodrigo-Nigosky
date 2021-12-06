88b7ba4bd3b1493a491c50e66046d51a4f52fcce
Rodrigo Nigosky <nigosky@yahoo.com.br>
2021-12-06 15:51:51 -0300

    update-objetos

---
+++ modulo1/objetos/index.js
@@ -0,0 +1,21 @@
+console.log ("hello world")
+
+//Exercícios de interpretação de código
+//exercicio 1. será imprenso: "Matheus Nachtergaele" , "Virginia Cavendish" "Globo", horario: "14h"
+
+// exercicio 2. a)
+/* 
+{nome: 'Juca', idade: 3, raca: 'SRD'}
+index.js:20 {nome: 'Juba', idade: 3, raca: 'SRD'}
+index.js:21 {nome: 'Jubo', idade: 3, raca: 'SRD'} */
+
+/* b) cria uma copia do objeto/array para manipular a copia sem alterar o objeto original */
+
+//exercicio 3
+//a) será impresso false e undefined
+
+// b) foi impresso o valor da chave backender e impresso um valor de uma chave que nao existe (no caso, altura), por isso apareceu undefined
+
+//Exercícios de escrita de código
+// 1a)
+
