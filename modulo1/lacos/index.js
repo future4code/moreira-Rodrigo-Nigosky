b9468beaad6032f047a54c6ebf434224978a8e98
Rodrigo Nigosky <nigosky@yahoo.com.br>
2021-12-08 16:46:49 -0300

    update-lacos

---
+++ modulo1/lacos/index.js
@@ -0,0 +1,103 @@
+console.log ("hello world")
+
+
+//Exercícios de interpretação de código
+
+//1) - o codigo está checando uma repetição,dentro de uma array e verificando quais são cinco numeros dentro dessa array. 
+//      O resultado impresso no console é: 10
+
+//2)
+//a.) será impresso todos os numeros acima de 18, no caso: [19, 21, 23, 25, 27, 30]
+
+//b.)
+
+
+//3) vai gerar a quantidade de 4 linhas e cada linha será a quantidade de asteristicos referente ao numero da linha
+
+
+//Exercícios de escrita de código
+//1)
+//a)
+
+/* 
+let pets = Number (prompt ('quantos pets voce tem?'))
+
+//b)
+if (pets === 0){
+    console.log("Que pena! Você pode adotar um pet!")
+}
+    if (pets > 0){
+        const listaDePets = []
+
+        for (let i = 0; i < pets; i++){
+            const PetsDoUsuario = prompt ('digite os nomes do seu pet um por um')
+            listaDePets.push(petsDoUsuario)
+        }
+
+//c)
+        console.log(listaDePets)
+    }
+
+                    
+ */
+
+
+
+//2)
+
+/* 
+const arrayOriginal = [05, 07, 10, 12, 21, 71]
+ */
+
+
+//a) 
+/* function imprimeValores (array){
+for(let valor of array)
+    console.log(valor)
+}
+imprimeValores(arrayOriginal) */
+
+
+
+//b) 
+/* 
+function imprimeValoresDivididosPorDez (array){
+
+    for(let valor of array){
+    let valorDividido = (valor/10)
+        console.log(valorDividido)
+    }    
+}
+imprimeValoresDivididosPorDez(arrayOriginal)
+ */
+
+
+
+//c)
+/* 
+function imprimePares(array){
+    const listaDePares = []
+
+    for(let numero of array){
+        if(numero % 2 === 0){
+            listaDePares.push(numero)
+        }
+    }
+
+console.log (listaDePares)
+
+}
+imprimePares(arrayOriginal) 
+
+
+ */
+
+
+//d)
+
+
+
+
+
+
+
